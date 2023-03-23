2b4e455cd11b3c72ad51774441e34dcbd64f6488
emoney17 <emmanuel.million@tutanota.com>
2023-03-23 16:03:54 -0500

    added kebinds and icon for bar

--- xmonad.hs
+++ xmonad.hs
@@ -1,13 +1,20 @@
 import XMonad
+import XMonad.Operations
+import System.Exit
+
+-- Actions
+import XMonad.Actions.WithAll
 
 -- Utils
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Util.Loggers
+import XMonad.Util.SpawnOnce
 
 -- Layouts
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Magnifier
+import XMonad.Layout.Renamed
 
 -- Hooks
 import XMonad.Hooks.EwmhDesktops
@@ -17,10 +24,15 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
+myTerminal	= "alacritty"
+myBrowser	= "firefox"
+myEmacs		= "emacsclient -c -a 'emacs' --eval '(dashboard-open)'"
+myDmenu		= "dmenu_run -fn 'monospace-11'"
+
 main :: IO ()
-main = xmonad 
-     . ewmhFullscreen 
-     . ewmh 
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
      . withEasySB (statusBarProp "xmobar ~/.config/xmonad/xmobarrc" (pure myXmobarPP)) toggleStrutsKey
      $ myConfig
    where
@@ -31,11 +43,19 @@ myConfig = def
 	{ modMask = mod4Mask	-- Change mod key
 	, layoutHook = myLayout	-- Use custom layout
 	, manageHook = myManageHook -- Match on certain windows
+	, startupHook = myStartupHook
+	, terminal = myTerminal
 	}
 	`additionalKeysP`
-	[ ("M-b", spawn "firefox")
-	, ("M-e", spawn "emacs")
-	, ("M-s", unGrab *> spawn "scrot -s")
+	[ ("M-b", 		    spawn myBrowser)
+	, ("M-<Return>", 	spawn myTerminal)
+	, ("M-e", 		    spawn myEmacs)
+	, ("M-d", 		    spawn myDmenu)
+	, ("M-q", 		    kill)
+	, ("M-S-r", 		spawn "xmonad --restart")
+	, ("M-s", 		    unGrab *> spawn "scrot -s")
+	, ("M-S-<Space>", 	sinkAll)
+	, ("M-S-e", 		io (exitWith ExitSuccess))
 	]
 
 myManageHook :: ManageHook
@@ -46,7 +66,9 @@ myManageHook = composeAll
 
 myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
     where
-    	threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio -- Magnification on threeCol
+    	threeCol = renamed [Replace "ThreeCol"]
+		$ magnifiercz' 1.3
+		$ ThreeColMid nmaster delta ratio -- Magnification on threeCol
     	-- threeCol = ThreeColMid nmaster delta ratio
     	tiled 		= Tall nmaster delta ratio
     	nmaster 	= 1	-- Number of masters
@@ -55,14 +77,14 @@ myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
 
 myXmobarPP :: PP
 myXmobarPP = def
-	{ ppSep 		= magenta " . "
-	, ppTitleSanitize 	= xmobarStrip
-	, ppCurrent 		= wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-	, ppHidden 		= white . wrap " " ""
+	{ ppSep 		        = magenta " . "
+	, ppTitleSanitize 	    = xmobarStrip
+	, ppCurrent 		    = wrap " " "" . xmobarBorder "Bottom" "#8be9fd" 2
+	, ppHidden 		        = white . wrap " " ""
 	, ppHiddenNoWindows 	= lowWhite . wrap " " ""
-	, ppUrgent 		= red . wrap (yellow "!") (yellow "!")
-	, ppOrder 		= \[ws, l, _, wins] -> [ws, l, wins] -- Show workspaces, layout name, window name
-	, ppExtras 		= [logTitles formatFocused formatUnfocused] -- Show title of all windows
+	, ppUrgent 	        	= red . wrap (yellow "!") (yellow "!")
+	, ppOrder 	        	= \[ws, l, _, wins] -> [ws, l, wins] -- Show workspaces, layout name, window name
+	, ppExtras 	        	= [logTitles formatFocused formatUnfocused] -- Show title of all windows
 	}
       where
       	formatFocused 	= wrap (white    "[") (white    "]") . magenta . ppWindow
@@ -77,5 +99,11 @@ myXmobarPP = def
 	blue		= xmobarColor "#bd93f9" ""
 	white		= xmobarColor "#f8f8f2" ""
 	yellow		= xmobarColor "#f1fa8c" ""
-	red		= xmobarColor "#ff5555" ""
+	red		    = xmobarColor "#ff5555" ""
 	lowWhite	= xmobarColor "#bbbbbb" ""
+
+myStartupHook :: X()
+myStartupHook = do
+	spawnOnce "picom &"
+	spawnOnce "nitrogen --restore &"
+	spawnOnce "emacs --daemon &"
